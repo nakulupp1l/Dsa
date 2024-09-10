a83b0498c7ff4ab5823c53683eac5b8ae844011b
nakulupp1l <uppalnakul14@gmail.com>
2024-09-10 13:07:21 +0530

    deletion and searching in trie

--- hashmap and tries/trie L2.cpp
+++ hashmap and tries/trie L2.cpp
@@ -36,9 +36,45 @@ void insertw(Trie* root,string str){
 	//recursion sambhal legga
 	insertw(child,str.substr(1));
 }
+
+bool search(Trie* root,string word){
+	if(word.length()==0) return root->isTerminal;
+	
+	char ch=word[0];
+	int i=ch-'a';
+	Trie* child;
+	if(root->children[i]!=NULL){
+		child=root->children[i];
+	}
+	else return 0;
+	return search(child,word.substr(1));
+}
+
+void delw(Trie* root,string str){
+	//base case
+	if(str.length()==0) {
+	 	root->isTerminal=0;
+	 	return;
+	}
+	//main code
+	char ch=str[0];
+	int i=ch-'a';
+	Trie* child;
+	if(root->children[i]!=NULL){
+		child=root->children[i];
+	}
+	else{
+		child=new Trie(ch);
+		root->children[i]=child;
+	}
+	//recursion sambhal legga
+	delw(child,str.substr(1));
+}
 int main(){
 	Trie* root=new Trie('-');
 	insertw(root,"coding");
 	insertw(root,"code");
+	delw(root,"coding");
+	cout<<search(root,"coding")<<" "<<search(root,"codin");
     return 0;
 }
